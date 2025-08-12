9c3c96ed24362a60c547b1df30d0d08015c8f918
Emmanuel Egomson <fegomson@gmail.com>
2025-08-11 23:47:39 -0500

    feat: add theme toggler and provider components, update layout to include AppProviders

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,13 @@
 import BreadCrumbHeader from "./BreadCrumbHeader"
+import { ModeToggler } from "./ModeToggler"
 
 const Header = () => {
     return (
         <header className='flex items-center justify-between px-6 py-4 h-[50px] container'>
             <BreadCrumbHeader />
+            <div className="gap-1 flex items-center">
+                <ModeToggler />
+            </div>
         </header>
     )
 }
