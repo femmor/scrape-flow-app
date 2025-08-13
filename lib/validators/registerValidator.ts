83db7f4efc42d9e0145497cb1ede5cab70bf749a
Emmanuel Egomson <fegomson@gmail.com>
2025-08-12 23:55:53 -0500

    feat: enhance login form with Zod validation, user sign-in functionality, and improved UI components

--- lib/validators/registerValidator.ts
+++ lib/validators/registerValidator.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const registerValidator = z.object({
     username: z.string().min(3, { message: "Username must be at least 3 characters long" }).max(100),
-    email: z.email(),
+    email: z.email({ message: "Invalid email address" }),
     password: z.string().min(8, { message: "Password must be at least 8 characters long" }),
     confirmPassword: z.string().max(100, { message: "Confirm Password must be at most 100 characters long" })
 }).refine(
--- server/users.ts
+++ server/users.ts
@@ -17,3 +17,19 @@ export const signUpUser = async (username: string, email: string, password: stri
         return { success: false, message: e.message || "Sign-up failed" };
     }
 }
+
+export const signInUser = async (email: string, password: string) => {
+    try {
+        await auth.api.signInEmail({
+            body: {
+                email,
+                password
+            }
+        })
+
+        return { success: true, message: "Sign in successful" }
+    } catch (error) {
+        const e = error as Error;
+        return { success: false, message: e.message || "Sign-in failed" };
+    }
+}
