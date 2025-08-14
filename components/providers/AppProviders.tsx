b5f4313f781df5c7cf3775b755da05db8b968b3d
Emmanuel Egomson <fegomson@gmail.com>
2025-08-13 23:08:04 -0500

    feat: integrate React Query for data fetching and add devtools support

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -2,16 +2,23 @@
 
 import { ReactNode } from "react"
 import { ThemeProvider } from "./theme-provider"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 export function AppProviders({ children }: { children: ReactNode }) {
+    const queryClient = new QueryClient();
+
     return (
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-        >
-            {children}
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+            </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
     )
 }
